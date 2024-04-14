babe86961bc9e237e8755796c454d347f64d2603
Abhijeet Sirohi <abhijeetsirohiajso@gmail.com>
2024-04-14 16:10:31 +0530

    classes def

---
+++ src/classes.cpp
@@ -0,0 +1,46 @@
+
+#include<iostream>
+using namespace std;
+
+class customer{
+public:
+     string cust_name;
+     string cust_mail;
+     string cust_passwd;
+     string cust_phone_no;
+     int discount;
+}
+
+
+class product{
+public:
+    int product_id;
+    string product_name;
+    int stock_quantity;
+    float price;
+    
+
+
+
+
+};
+
+class clothes:public{
+public:
+    string gender;
+    string csize;
+    string brand;
+    string 
+
+
+
+};
+    
+    
+    
+
+
+
+
+
+};
