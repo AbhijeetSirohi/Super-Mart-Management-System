e5146d9ee0ce9cb1de578262f17e046560d0b7e3
Abhijeet Sirohi <abhijeetsirohiajso@gmail.com>
2024-04-15 16:34:08 +0530

    classes1

--- src/classes.cpp
+++ src/classes.cpp
@@ -17,8 +17,9 @@ public:
     int product_id;
     string product_name;
     int stock_quantity;
-    float price;
-    
+    float selling_price;
+    float cost_price;
+    int items_sold;/*quantity sold*/
 
 
 
@@ -30,7 +31,66 @@ public:
     string gender;
     string csize;
     string brand;
+    string colour;
     
+};
+
+class books{
+public:
+   string author;
+   string genre;
+
+
+
+};
+
+class food{
+    public:
+    string category;  /*veg or nonveg*/
+    string brand;
+
+
+
+};
+
+class shoes{
+    public:
+    string brand;
+    int size;
+    string gender;
+
+
+};
+
+class medicine{
+    public:
+    string disease;
+    float weight; /*weight in mg*/
+
+
+};
+
+class staff{
+    public:
+    int staff_id;
+    string staff_name;
+    string gender;
+    float salary;
+    string staff_phone_no;
+    string staff_mail;
+
+
+    
+
+};
+
+class manager:public staff{
+public:
+void pos();/*star product and bills*/
+void inventory();/**/
+void view_staff_details();
+void terminate_add_staff();
+void customerdetails();
 
 
 
