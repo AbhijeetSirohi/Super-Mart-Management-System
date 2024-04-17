4a8062145d6c38de5d2dea9acf4f2b8394abd075
Gunaj0517 <gunaj.chugh@gmail.com>
2024-04-17 23:05:22 +0530

    staff class changes

--- src/classes.cpp
+++ src/classes.cpp
@@ -181,7 +181,8 @@ class healthcare:public product{
 
 class staff{
     public:
-    int staff_id;
+    string staff_id;
+    string staff_pswd;
     string staff_name;
     string gender;
     float salary;
@@ -190,8 +191,8 @@ class staff{
    string staff_type;
 
 
-staff(int id, const std::string& name, const std::string& gen, float sal, const std::string& phone_no, const std::string& mail, const std::string& type) :
-        staff_id(id),
+staff(const string id, const string pswd, const std::string& name, const std::string& gen, float sal, const std::string& phone_no, const std::string& mail, const std::string& type) :
+        staff_id(id),staff_pswd(pswd),
         staff_name(name),
         gender(gen),
         salary(sal),
